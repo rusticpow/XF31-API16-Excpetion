1488658c21b7d698c98d319362cc8b3341ac22fa
Rishat Zakirov <rzakirovt@gmail.com>
2018-06-23 14:52:49 +0300

    Init

---
+++ XF31Test/XF31Test/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XF31Test.Models;
+
+namespace XF31Test.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ XF31Test/XF31Test/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.Xaml;
+using TabbedPage = Xamarin.Forms.TabbedPage;
+
+namespace XF31Test.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : TabbedPage
+	{
+		public MainPage ()
+		{
+		    On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+			InitializeComponent ();
+		}
+	}
+}
